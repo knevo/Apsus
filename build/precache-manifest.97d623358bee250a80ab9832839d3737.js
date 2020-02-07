73a5fca872dccd9fb8f7adb79a74de67181fb429
knevo <nevok18@gmail.com>
2020-02-07 17:41:09 +0200

    build

---
+++ build/precache-manifest.97d623358bee250a80ab9832839d3737.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73c001418afae3ab1194c771c05578a",
+    "url": "/talazenkot/Appsus/index.html"
+  },
+  {
+    "revision": "04d2454abea26c48adf3",
+    "url": "/talazenkot/Appsus/static/css/main.37164905.chunk.css"
+  },
+  {
+    "revision": "da157c1ab4d6857738e5",
+    "url": "/talazenkot/Appsus/static/js/2.10335be9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/talazenkot/Appsus/static/js/2.10335be9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d2454abea26c48adf3",
+    "url": "/talazenkot/Appsus/static/js/main.8eb991ed.chunk.js"
+  },
+  {
+    "revision": "9345efec234b191f3835",
+    "url": "/talazenkot/Appsus/static/js/runtime-main.32c0d6ca.js"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/talazenkot/Appsus/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/talazenkot/Appsus/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/talazenkot/Appsus/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "d16270f7d102b057b267a67e306d408d",
+    "url": "/talazenkot/Appsus/static/media/books.d16270f7.jpg"
+  },
+  {
+    "revision": "4849fb91df3e50941a0f870f55941359",
+    "url": "/talazenkot/Appsus/static/media/keep.4849fb91.jpg"
+  },
+  {
+    "revision": "845441bf5653a904f77ed0a0bc5d6a68",
+    "url": "/talazenkot/Appsus/static/media/mail.845441bf.jpg"
+  },
+  {
+    "revision": "595c59ededc000737f9c67cf2a0900f2",
+    "url": "/talazenkot/Appsus/static/media/main.595c59ed.jpg"
+  }
+]);
